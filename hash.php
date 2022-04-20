a7b92992f11654169266967d7c3981b453cd65d0
pyae phyo thant <96177991+coderpyaephyothant@users.noreply.github.com>
2022-04-20 07:44:08 +0630

    Update hash.php

--- hash.php
+++ hash.php
@@ -1,4 +1,4 @@
 <!-- password hash sample -->
 
 password_hash('password',PASSWORD_DEFAULT);
-password_verify($password,$user['password']);
+password_verify($password,$user['password']); // Post_password, Database password!
